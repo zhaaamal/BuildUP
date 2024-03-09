e37d139d6581f2103a8b1701dd9be17e6f46717f
zhaaamal <madiarovanurjamal@gmail.com>
2024-03-09 19:12:44 +0600

    urls.py/core

--- core/urls.py
+++ core/urls.py
@@ -5,8 +5,9 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include("content.urls")),
+    path('api/content/', include("content.urls")),
     path('api/user/', include('users.urls')),
+    path('api/product/', include('product.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
