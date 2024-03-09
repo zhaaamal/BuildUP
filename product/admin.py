e34d59978f0d04d48c50ba84a9c8d73bb6b7ff6f
zhaaamal <madiarovanurjamal@gmail.com>
2024-03-09 20:21:20 +0600

    add swagger

--- product/admin.py
+++ product/admin.py
@@ -2,5 +2,18 @@ from django.contrib import admin
 
 from .models import Category, Product
 
-admin.site.register(Category)
-admin.site.register(Product)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+    list_filter = ('title',)
+    search_fields = ('title',)
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('title', 'img', 'price', )
+    list_filter = ('title', 'category', 'price')
+    search_fields = ('title', 'category', 'price')
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product, ProductAdmin)
